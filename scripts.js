a972c96d7371502a6b798acf653f691d11f82ea7
GuilhermeLimaDev <guilhermeguiguigugu2509@gmail.com>
2024-03-20 08:28:37 -0300

    Consumo API viacep

--- scripts.js
+++ scripts.js
@@ -15,7 +15,20 @@ var saida = document.getElementById("saida-de-dados");
 
 function alertar(event){
     //alert("Você clicou no botão!!!" + nome.value);
-    saida.innerText= "Nome: " + nome.value +
+
+
+    const url = `https://viacep.com.br/ws/${cep.value}/json`;
+     
+    fetch(url)
+    .then(resposta=>resposta.json())
+    .then(dados=>{
+
+        log.value= dados.logradouro;
+        bairro.value= dados.bairro;
+        cidade.value= dados.localidade;
+        estado.value= dados.uf;
+
+        saida.innerText= "Nome: " + nome.value +
     "\n Email: " + email.value +
     "\n Telefone: " + telefone.value +
     "\n CEP: " + cep.value +
@@ -25,4 +38,9 @@ function alertar(event){
     "\n Bairro: " + bairro.value +
     "\n Cidade: " + cidade.value +
     "\n Estado: " + estado.value;
+
+    })
+    
+
+    
 }
